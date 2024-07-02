5e6578f5c2ef0a5d232dbd711d484ae01c32820a
MnrMine <mnr_mine_agri@hotmail.com>
2024-07-02 08:30:05 +0300

    Add project files.

---
+++ AjaxDay/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxDay.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-HHOE249\\SQLEXPRESS;initial catalog=AjaxDb;integrated Security=true;");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
